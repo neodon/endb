467a101ff65d3a6115ef1d56d95a7502c1ba4664
chroventer <chroventer@gmail.com>
2019-09-29 16:35:50 +0400

    Linted and beautified the code

--- src/adapters/mysql.js
+++ src/adapters/mysql.js
@@ -5,22 +5,22 @@ const { safeRequire } = require('../util');
 const { createConnection } = safeRequire('mysql2/promise');
 
 class EndbMysql extends EndbSql {
-  constructor(options = {}) {
-    if (typeof options === 'string') {
-      options = { uri: options };
+    constructor(options = {}) {
+        if (typeof options === 'string') {
+            options = { uri: options };
+        }
+        options = Object.assign({
+            dialect: 'mysql',
+            uri: 'mysql://localhost',
+        }, options);
+        options.connect = () => Promise.resolve()
+            .then(() => createConnection(options.uri))
+            .then(connection => {
+                return sql => connection.execute(sql)
+                    .then(data => data[0]);
+            });
+        super(options);
     }
-    options = Object.assign({
-      dialect: 'mysql',
-      uri: 'mysql://localhost',
-    }, options);
-    options.connect = () => Promise.resolve()
-      .then(() => createConnection(options.uri))
-      .then(connection => {
-        return sql => connection.execute(sql)
-          .then(data => data[0]);
-      });
-    super(options);
-  }
 }
 
 module.exports = EndbMysql;
--- src/adapters/postgres.js
+++ src/adapters/postgres.js
@@ -5,21 +5,21 @@ const { safeRequire } = require('../util');
 const { Pool } = safeRequire('pg');
 
 class EndbPostgres extends EndbSql {
-  constructor(options = {}) {
-    options = Object.assign({
-      dialect: 'postgres',
-      uri: 'postgresql://localhost:5432',
-    }, options);
-    options.connect = () => Promise.resolve()
-      .then(() => {
-        const pool = new Pool({
-          connectionString: options.uri,
-        });
-        return sql => pool.query(sql)
-          .then(data => data.rows);
-      });
-    super(options);
-  }
+    constructor(options = {}) {
+        options = Object.assign({
+            dialect: 'postgres',
+            uri: 'postgresql://localhost:5432',
+        }, options);
+        options.connect = () => Promise.resolve()
+            .then(() => {
+                const pool = new Pool({
+                    connectionString: options.uri,
+                });
+                return sql => pool.query(sql)
+                    .then(data => data.rows);
+            });
+        super(options);
+    }
 }
 
 module.exports = EndbPostgres;
