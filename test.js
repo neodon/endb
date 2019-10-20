54dab56513dae605a18af9b810222dc2005a395c
chroventer <chroventer@gmail.com>
2019-10-20 20:02:02 +0400

    test: remove a manual test

--- test.js
+++
@@ -1,9 +0,0 @@
-const Endb = require('./');
-const endb = new Endb('mongodb+srv://test:test1234567890@test-rhvos.mongodb.net/test?retryWrites=true&w=majority', { adapter: 'mongodb'});
-
-endb.on('error', console.error);
-
-endb.get('key');
-endb.set('key', 'value');
-
-console.log(endb.options.store);
