2b62b7cf6a8b4e11146638159f7d636c8ad8cae3
chroventer <ayyanlewis@gmail.com>
2019-11-13 22:37:37 +0400

    fix(types): new interface Element

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,30 +2,35 @@ declare module 'endb' {
   import { EventEmitter } from 'events';
   export const version: string;
 
-  export type EndbOptions = {
-    namespace: string;
+  type EndbOptions = {
+    namespace: string | 'endb';
     serialize: Function;
     deserialize: Function;
-    adapter?: string;
+    adapter: 'level' | 'leveldb' | 'mongo' | 'mongodb' | 'mysql' | 'mysql2' | 'postgres' | 'postgresql' | 'redis' | 'sqlite' | 'sqlite3';
     store: any;
-    collection?: string | 'endb';
-    table?: string | 'endb';
-    keySize?: number | 255;
-  };
+    collection: string | 'endb';
+    table: string | 'endb';
+    keySize: number | 255;
+  }
+
+  interface Element {
+    key: string;
+    value: any;
+  }
 
   export class Endb extends EventEmitter {
-    public options?: EndbOptions;
-    constructor(uri?: string, options?: EndbOptions);
-    public all(): Promise<any[]>;
+    public options: EndbOptions;
+    constructor(uri: string, options: EndbOptions);
+    public all(): Promise<Element[]>;
     public clear(): Promise<undefined>;
     public delete(key: string): Promise<true>;
-    public async find(fn: Function, thisArg: any): Promise<Object<any> | undefined>;
+    public find(fn: Function, thisArg: any): Promise<Element | undefined>;
     public get(key: string): Promise<any>;
-    public async has(key: string): Promise<boolean>;
-    public async math(key: string, operation: string, operand: number): Promise<true>;
-    public static multi(names: string[], options?: EndbOptions): Object<Endb>;
+    public has(key: string): Promise<boolean>;
+    public math(key: string, operation: string, operand: number): Promise<true>;
+    public static multi(names: string[], options: EndbOptions): any;
     public set(key: string, value: any): Promise<true>;
-  };
+  }
 
   export class Util {
     public static math(base: number, op: string, opand: number): number;
