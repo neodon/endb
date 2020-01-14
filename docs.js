4f510a712b91ecabf7b57ed628373f231539d8e4
chroventer <ayyanlewis@gmail.com>
2020-01-14 10:38:06 -0700

    docs: switch to docma

---
+++ docs.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Docma = require('docma');
+const Package = require('./package');
+
+Docma
+    .create()
+    .build({
+        app: {
+            title: Package.name,
+            routing: 'query',
+            entrance: 'content:readme',
+            base: '/docs',
+            server: Docma.ServerType.GITHUB
+        },
+        markdown: {
+            sanitize: false
+        },
+        src: [
+            { endb: './src/index.js' },
+            { readme: './README.md' },
+        ],
+        dest: './docs',
+        template: {
+            options: {
+                title: Package.name,
+                navbar: true,
+                sidebar: true,
+                navItems: [{
+                        label: 'README',
+                        href: '?content=readme',
+                    },
+                    {
+                        label: 'Documentation',
+                        href: '?api=endb',
+                        iconClass: 'ico-book',
+                    },
+                    {
+                        label: 'GitHub',
+                        href: Package.repository.url,
+                        target: '_blank',
+                        iconClass: 'ico-md ico-github',
+                    },
+                ],
+            },
+        }
+    }).catch(console.error);
