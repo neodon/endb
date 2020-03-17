9278e71a1bed3bc41dfc0681cf91e382b03db88a
chroventer <ayyanlewis@gmail.com>
2020-03-17 02:50:30 -0700

    test(cache): undeclared variable

--- test/cache.test.js
+++ test/cache.test.js
@@ -2,7 +2,8 @@ const test = require('ava');
 const Endb = require('../src');
 
 test.serial('Adapters (Cache)', async t => {
-	const endb = new Endb({ store: new Map() });
+	const store = new Map();
+	const endb = new Endb({ store });
 	t.is(store.size, 0);
 	t.is(await endb.set('foo', 'bar'), true);
 	t.is(await endb.get('foo'), 'bar');
