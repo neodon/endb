0e515edfad83c96bdcfe82b69ad399594260c6bb
chroventer <ayyanlewis@gmail.com>
2020-03-17 02:47:25 -0700

    refactor(test): remove object destructing

--- test/cache.test.js
+++ test/cache.test.js
@@ -2,8 +2,7 @@ const test = require('ava');
 const Endb = require('../src');
 
 test.serial('Adapters (Cache)', async t => {
-	const store = new Map();
-	const endb = new Endb({store});
+	const endb = new Endb({ store: new Map() });
 	t.is(store.size, 0);
 	t.is(await endb.set('foo', 'bar'), true);
 	t.is(await endb.get('foo'), 'bar');
--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,7 @@ const test = require('ava');
 const Endb = require('../src');
 
 test.serial('Class', t => {
-	console.log(typeof Endb);
 	t.is(typeof Endb, 'function');
-	t.throws(() => Endb()); // eslint-disable-line new-cap
+	t.throws(() => Endb());
 	t.notThrows(() => new Endb());
 });
