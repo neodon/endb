ee811d3cd9dd9ae84f293345a4cd77de4d6d0ee3
chroventer <ayyanlewis@gmail.com>
2020-03-17 02:48:13 -0700

    refactor(test): remove object destructuring

--- test/multi.test.js
+++ test/multi.test.js
@@ -2,13 +2,13 @@ const test = require('ava');
 const Endb = require('../src');
 
 test.serial('Multiple Endb instances', async t => {
-	const {members, users} = Endb.multi(['members', 'users']);
-	t.is(await members.set('foo', 'bar'), true);
-	t.is(await users.set('bar', 'foo'), true);
-	t.is(await members.has('foo'), true);
-	t.is(await users.has('bar'), true);
-	t.is(await members.get('foo'), 'bar');
-	t.is(await users.get('bar'), 'foo');
-	t.is(await members.clear(), undefined);
-	t.is(await users.clear(), undefined);
+	const endb = Endb.multi(['members', 'users']);
+	t.is(await endb.members.set('foo', 'bar'), true);
+	t.is(await endb.users.set('bar', 'foo'), true);
+	t.is(await endb.members.has('foo'), true);
+	t.is(await endb.users.has('bar'), true);
+	t.is(await endb.members.get('foo'), 'bar');
+	t.is(await endb.users.get('bar'), 'foo');
+	t.is(await endb.members.clear(), undefined);
+	t.is(await endb.users.clear(), undefined);
 });
