f3abbcff1d25267261577fcf6ed77875fc4b172b
Richard <37025447+MysticCraftYT@users.noreply.github.com>
2020-04-17 14:31:27 +0200

    feat: add connectTimeout option to mysql adapter (#53)
    * Increased Timeout
    
    Increased the MySQL connection timeout to 30 seconds, 10 seconds is too low
    
    * Added the option to choose the connectTimeout
    
    * fix: remove safeRequire method
    
    * chore: change the default timeout to 10000
    
    * chore: make password optional
    
    * chore: make password and port optional
    
    * chore: make password and port optional
    
    * chore: add connectTimeout option
    
    Co-authored-by: Ayyan Lewis <ayyanlewis@gmail.com>

--- src/adapters/mysql.js
+++ src/adapters/mysql.js
@@ -5,17 +5,24 @@ const Sql = require('./sql');
 
 module.exports = class MySQL extends Sql {
 	constructor(options = {}) {
-		const {uri = 'mysql://localhost'} = options;
+		const options_ = {
+			uri: 'mysql://localhost',
+			...options
+		};
 		super({
 			dialect: 'mysql',
 			async connect() {
-				const connection = await mysql.createConnection(uri);
+				if (options_.connectTimeout) {
+					options_.uri += `?connectTimeout=${Number(options.connectTimeout)}`;
+				}
+
+				const connection = await mysql.createConnection(options_.uri);
 				return async (sqlString) => {
 					const [row] = await connection.execute(sqlString);
 					return row;
 				};
 			},
-			...options
+			...options_
 		});
 	}
 };
